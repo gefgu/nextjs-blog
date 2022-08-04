d3730d6d539e9f642f8042e2ee093f781935cb79
gefgu <gefgu@hotmail.com>
2022-08-04 15:05:09 -0300

    Add component

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -2,10 +2,11 @@ import Link from "next/link";
 import Image from "next/image";
 import Head from "next/head";
 import Script from "next/script";
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
       <Head>
         <title>First Post</title>
       </Head>
@@ -28,6 +29,6 @@ export default function FirstPost() {
           <a>Back To home</a>
         </Link>
       </h2>
-    </>
+    </Layout>
   );
 }
